dadad0d4f98d7e2c995e6e1651fad5ddf2d07fba
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-03-14 09:40:40 +0000

    add exercise 4

--- Special/Program.cs
+++ Special/Program.cs
@@ -4,9 +4,30 @@ namespace Special
 {
     public class Program
     {
+        private static int GetSpecial(int n)
+        {
+            if (n == 0)  
+            {
+                return 0;
+            } 
+            else if (n == 1) 
+            {
+                return 1;
+            } 
+            else  
+            {
+                return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+            }
+        }
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("The {0}-th special number is: {1}", n, GetSpecial(n));
         }
     }
 }
+
+
+
