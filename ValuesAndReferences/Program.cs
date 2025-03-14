77f92302dd9c72249983ee7f02987f39b768ebee
sabrinasilva26 <sabrinasilva.art@gmail.com>
2025-03-14 09:47:34 +0000

    Add exercise 6

---
+++ ValuesAndReferences/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValuesAndReferences
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
